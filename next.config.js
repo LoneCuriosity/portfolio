ce6d22bcf03eaef8b9b6a4f6a3e6c2a0b10cd183
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-07-16 20:28:19 -0500

    updating packages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        // The `src` property hostname must end with `.example.com`,
-        // otherwise the API will respond with 400 Bad Request.
-        protocol: 'https',
-        hostname: '**.githubusercontent.com',
-      },
-    ],
-  },
 }
 
 module.exports = nextConfig
