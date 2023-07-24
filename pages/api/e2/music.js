bec8e77f6e11e4e4ed6e41524c43f797fb4da33e
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-07-23 21:53:39 -0500

    update music api testing pipe instead of write

--- pages/api/e2/music.js
+++ pages/api/e2/music.js
@@ -7,10 +7,11 @@ export default function handler(req, res) {
     if(ytdl.validateURL(URL)){
         let stream = ytdl(URL,{ quality: 'highestaudio' })
 
+        //res.setHeader('Content-Disposition', `attachment; filename=song.mp3`);
         ffmpeg(stream)
             .audioBitrate(128)
             .format('mp3')
-            .writeToStream(res, { end: true })
+            .pipe(res, { end: true });
     } else {
         res.status(400).json({"ERROR":"Not valid URL"})
     }
