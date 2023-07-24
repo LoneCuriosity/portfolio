5fb7d24f103f0cd47fa0a3581437b380ef22ead1
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-07-23 21:23:07 -0500

    update music api to use mp3 format

--- pages/api/e2/music.js
+++ pages/api/e2/music.js
@@ -1,9 +1,16 @@
 const ytdl = require('ytdl-core')
+const ffmpeg = require('fluent-ffmpeg');
 
 export default function handler(req, res) {
     const { URL } = req.query
+
     if(ytdl.validateURL(URL)){
-        ytdl(URL,{ filter: 'audioonly'}).pipe(res)
+        let stream = ytdl(URL,{ quality: 'highestaudio' })
+
+        ffmpeg(stream)
+            .audioBitrate(128)
+            .format('mp3')
+            .writeToStream(res, { end: true })
     } else {
         res.status(400).json({"ERROR":"Not valid URL"})
     }
