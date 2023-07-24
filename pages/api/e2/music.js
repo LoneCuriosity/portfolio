fb7d9b5b9b446898333882bf4c6f975894d6927f
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-07-23 21:05:07 -0500

    update music api to audio only

--- pages/api/e2/music.js
+++ pages/api/e2/music.js
@@ -3,7 +3,7 @@ const ytdl = require('ytdl-core')
 export default function handler(req, res) {
     const { URL } = req.query
     if(ytdl.validateURL(URL)){
-        ytdl(URL).pipe(res)
+        ytdl(URL,{ quality: 'audioonly', format: 'webm' }).pipe(res)
     } else {
         res.status(400).json({"ERROR":"Not valid URL"})
     }
