69c4f07d75556f6e024350eeb4ca2626c0b54e70
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-07-23 21:56:36 -0500

    update music api set header

--- pages/api/e2/music.js
+++ pages/api/e2/music.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
     if(ytdl.validateURL(URL)){
         let stream = ytdl(URL,{ quality: 'highestaudio' })
 
-        //res.setHeader('Content-Disposition', `attachment; filename=song.mp3`);
+        res.setHeader('Content-Disposition', `attachment; filename=song.mp3`);
         ffmpeg(stream)
             .audioBitrate(128)
             .format('mp3')
