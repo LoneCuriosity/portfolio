46c0e18af05b5e485472625a1039d18a3f3fa38f
LoneCuriosity <ramon@ramongarciajr.tech>
2024-06-24 01:25:21 -0500

    removed dead script import

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script src="https://www.youtube.com/iframe_api"></script>
         <link rel="shortcut icon" href="/me.png" />
       </Head>
       <body>
